c7c5a54f399f9613aa58149816aca2517c5d1a15
AdrianHsu <adrianhsu1995@gmail.com>
2018-05-13 01:12:42 +0800

    fix

--- 10.sh
+++ 10.sh
@@ -1,8 +1,7 @@
 sudo mkdir /repo
 printf "\n[nasa-repo]\nSigLevel = Optional TrustAll\nServer = file:///repo" | sudo tee -a /etc/pacman.conf 
-sudo cp ./sudo-oasis* /repo
-cd /repo
-sudo repo-add /repo/nasa-repo.db.tar.gz sudo-oasis-0.1-1-any.pkg.tar.gz
+sudo cp sudo-oasis* /repo
+sudo repo-add /repo/nasa-repo.db.tar.gz /repo/sudo-oasis-0.1-1-any.pkg.tar.gz
 
 sudo gpg --import public_key.asc
 sudo pacman-key --add public_key.asc
--- init.sh
+++ init.sh
@@ -1,13 +1,16 @@
-sudo pacman -Sy git
-git clone https://github.com/AdrianHsu/makepkg.git
+# sudo pacman -Sy git
+# git clone https://github.com/AdrianHsu/makepkg.git
+cd makepkg
 gpg --full-gen-key
 # key: 91C81FFBC3EE8E05
-sudo gpg --output public_key.asc --armor --export b03901023
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+gpg --output public_key.asc --armor --export b03901023
 PACKAGER='b03901023 <b03901023@ntu.edu.tw>' makepkg --sign
 # passphrase: nasa2018
 
 pacman -Qip sudo-oasis-0.1.1-any.pkg.tar.gz
-sudo gpg --import public_key.asc
+gpg --import public_key.asc
 sudo pacman-key --add public_key.asc
 
 # sudo pacman-key --finger mykey
