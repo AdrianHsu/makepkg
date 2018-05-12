e978358517137c4c2ef10d131687901014025775
AdrianHsu <adrianhsu1995@gmail.com>
2018-05-13 01:29:05 +0800

    tar sleep wait

--- 10.sh
+++ 10.sh
@@ -1,5 +1,6 @@
 sudo mkdir /repo
 sudo cp sudo-oasis* /repo
+sleep 3 # to ensure copy files
 sudo repo-add /repo/nasa-repo.db.tar.gz /repo/sudo-oasis-0.1-1-any.pkg.tar.gz
 printf "\n[nasa-repo]\nSigLevel = Optional TrustAll\nServer = file:///repo" | sudo tee -a /etc/pacman.conf 
 
