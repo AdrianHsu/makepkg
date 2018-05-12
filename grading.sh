a9d703bb4344c45bfa0295c28ee25b04418a834d
AdrianHsu <adrianhsu1995@gmail.com>
2018-05-13 01:51:20 +0800

    no confirm

--- grading.sh
+++ grading.sh
@@ -3,6 +3,6 @@ tar xvf 10.tar && sudo ./10.sh
 sudo pacsync nasa-repo
 ls /repo
 pacman -Si sudo-oasis | grep Repository
-sudo pacman -S sudo-oasis
+sudo pacman --noconfirm -S sudo-oasis
 pacman -Qi sudo-oasis | grep Validated
 pacman -Qi sudo-oasis | grep Packager
