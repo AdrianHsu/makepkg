ffeb43e2c769caf28af16f7d3879eb29940e86df
AdrianHsu <adrianhsu1995@gmail.com>
2018-05-13 01:24:53 +0800

    try again

--- 10.sh
+++ 10.sh
@@ -1,7 +1,7 @@
 sudo mkdir /repo
-printf "\n[nasa-repo]\nSigLevel = Optional TrustAll\nServer = file:///repo" | sudo tee -a /etc/pacman.conf 
 sudo cp sudo-oasis* /repo
 sudo repo-add /repo/nasa-repo.db.tar.gz /repo/sudo-oasis-0.1-1-any.pkg.tar.gz
+printf "\n[nasa-repo]\nSigLevel = Optional TrustAll\nServer = file:///repo" | sudo tee -a /etc/pacman.conf 
 
 sudo gpg --import public_key.asc
 sudo pacman-key --add public_key.asc
